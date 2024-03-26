9cc6e5c1c89d941a87fe7fa313a303aac7d97dfc
kill4us <1204330998@qq.com>
2024-03-26 20:21:30 +0800

    feature:通过分布式锁，防止恶意请求注册

---
+++ admin/src/main/java/com/kill4us/shortlink/admin/common/constant/RedisCacheConstant.java
@@ -0,0 +1,8 @@
+package com.kill4us.shortlink.admin.common.constant;
+
+/**
+ * Redis缓存常量类
+ */
+public class RedisCacheConstant {
+    public static final String LOCK_USER_REGISTER_KEY = "short-link:lock_user-register:";
+}
--- admin/src/main/java/com/kill4us/shortlink/admin/service/Impl/UserServiceImpl.java
+++ admin/src/main/java/com/kill4us/shortlink/admin/service/Impl/UserServiceImpl.java
@@ -1,25 +1,37 @@
 package com.kill4us.shortlink.admin.service.Impl;
 
+import cn.hutool.core.bean.BeanUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.kill4us.shortlink.admin.common.convention.exception.ClientException;
 import com.kill4us.shortlink.admin.dao.entity.UserDO;
 import com.kill4us.shortlink.admin.dao.mapper.UserMapper;
+import com.kill4us.shortlink.admin.dto.req.UserRegisterReqDTO;
 import com.kill4us.shortlink.admin.dto.resp.UserRespDTO;
 import com.kill4us.shortlink.admin.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.apache.catalina.User;
+import org.redisson.api.RBloomFilter;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import java.sql.Wrapper;
 
-import static com.kill4us.shortlink.admin.common.enums.UserErrorCodeEnum.USER_NOT_EXISTS;
+import static com.kill4us.shortlink.admin.common.constant.RedisCacheConstant.LOCK_USER_REGISTER_KEY;
+import static com.kill4us.shortlink.admin.common.enums.UserErrorCodeEnum.*;
 
 /**
  * 用户接口实现层
  */
 @Service
+@RequiredArgsConstructor
 public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements UserService {
+
+    private final RBloomFilter<String> userRegisterCachePenetrationBloomFilter;  //  注入布隆过滤器
+    private final RedissonClient redissonClient;
 
     @Override
     public UserRespDTO getUserByUsername(String username) {
@@ -32,5 +44,31 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements 
         }
         BeanUtils.copyProperties(userDO, result);
         return result;
+    }
+
+    @Override
+    public boolean hasUsername(String username) {
+        return !userRegisterCachePenetrationBloomFilter.contains(username);
+    }
+
+    @Override
+    public void register(UserRegisterReqDTO requestParam) {
+        if (!hasUsername(requestParam.getUsername())) {
+            throw new ClientException(USER_NAME_EXISTS);
+        }
+        RLock lock = redissonClient.getLock(LOCK_USER_REGISTER_KEY + requestParam.getUsername());
+        try {
+            if (lock.tryLock()) {  //  认为获取到锁的用户一定能注册成功，只有trylock成功了，才注册
+                int inserted = baseMapper.insert(BeanUtil.toBean(requestParam, UserDO.class));
+                if (inserted < 1) {
+                    throw new ClientException(USER_SAVE_ERROR);
+                }
+                userRegisterCachePenetrationBloomFilter.add(requestParam.getUsername());
+                return;
+            }
+            throw new ClientException(USER_NAME_EXISTS);
+        } finally {
+            lock.unlock();
+        }
     }
 }
