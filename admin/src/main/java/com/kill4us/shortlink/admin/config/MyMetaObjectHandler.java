971b9b89398823b698ebbe920f71ff0476a58177
kill4us <1204330998@qq.com>
2024-03-26 20:22:14 +0800

    feature:实现数据库填充日期

---
+++ admin/src/main/java/com/kill4us/shortlink/admin/config/MyMetaObjectHandler.java
@@ -0,0 +1,26 @@
+package com.kill4us.shortlink.admin.config;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        /**
+         * 填充t_user表中的createTime, updateTime, delFlag
+         */
+        this.strictInsertFill(metaObject, "createTime", Date::new, Date.class);
+        this.strictInsertFill(metaObject, "updateTime", Date::new, Date.class);
+        this.strictInsertFill(metaObject, "delFlag", Integer.class, 0);
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        this.strictInsertFill(metaObject, "updateTime", Date::new, Date.class);
+    }
+}
