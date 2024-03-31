ebfa5eca5f69de754b53094bc5f432468149075f
kill4us <1204330998@qq.com>
2024-03-31 20:30:36 +0800

    短链接分页接口新增创建时间

--- project/src/main/java/com/kill4us/shortlink/project/dto/resp/ShortLinkPageRespDTO.java
+++ project/src/main/java/com/kill4us/shortlink/project/dto/resp/ShortLinkPageRespDTO.java
@@ -1,6 +1,8 @@
 package com.kill4us.shortlink.project.dto.resp;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 
@@ -49,6 +51,12 @@ public class ShortLinkPageRespDTO {
      * 有效期
      */
     private Date validDate;
+
+    /**
+     * 创建时间
+     */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date createTime;
 
     /**
      * 描述
