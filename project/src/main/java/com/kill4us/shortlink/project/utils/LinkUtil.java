921469d5d27ae6da1781d725741ef580882f84ae
kill4us <1204330998@qq.com>
2024-04-07 16:29:24 +0800

    feature:实现短链接跳转uip监控

--- project/src/main/java/com/kill4us/shortlink/project/utils/LinkUtil.java
+++ project/src/main/java/com/kill4us/shortlink/project/utils/LinkUtil.java
@@ -2,6 +2,9 @@ package com.kill4us.shortlink.project.utils;
 
 import cn.hutool.core.date.DateUnit;
 import cn.hutool.core.date.DateUtil;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.Data;
 
 import javax.swing.text.html.Option;
@@ -14,6 +17,7 @@ public class LinkUtil {
 
     /**
      * 获取短链接有效期
+     *
      * @param validDate
      * @return
      */
@@ -21,5 +25,22 @@ public class LinkUtil {
         return Optional.ofNullable(validDate)
                 .map(each -> DateUtil.between(new Date(), each, DateUnit.MS))
                 .orElse(DEFAULT_CACHE_VALID_TIME);
+    }
+
+    /**
+     * 获取用户真实ip
+     */
+    public static String getActualIp(HttpServletRequest request) {
+        String ipAddress = request.getHeader("X-Forwarded-For");
+        if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getHeader("Proxy-Client-IP");
+        }
+        if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getRemoteAddr();
+        }
+        return ipAddress;
     }
 }
