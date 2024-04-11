c340de0dd5e179aadddf98c05a9ffd1a8a0c496e
kill4us <1204330998@qq.com>
2024-04-11 20:35:31 +0800

    feature:实现用户最多创建20个分组

--- admin/src/main/java/com/kill4us/shortlink/admin/common/constant/RedisCacheConstant.java
+++ admin/src/main/java/com/kill4us/shortlink/admin/common/constant/RedisCacheConstant.java
@@ -4,5 +4,14 @@ package com.kill4us.shortlink.admin.common.constant;
  * Redis缓存常量类
  */
 public class RedisCacheConstant {
+
+    /**
+     * 用户注册分布式锁
+     */
     public static final String LOCK_USER_REGISTER_KEY = "short-link:lock_user-register:";
+
+    /**
+     * 分组创建分布式锁
+     */
+    public static final String LOCK_GROUP_CREATE_KEY = "short-link:lock_group-create:%s";
 }
--- admin/src/main/java/com/kill4us/shortlink/admin/service/Impl/GroupServiceImpl.java
+++ admin/src/main/java/com/kill4us/shortlink/admin/service/Impl/GroupServiceImpl.java
@@ -1,6 +1,7 @@
 package com.kill4us.shortlink.admin.service.Impl;
 
 import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.collection.CollUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.baomidou.mybatisplus.core.toolkit.BeanUtils;
@@ -8,6 +9,7 @@ import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.kill4us.shortlink.admin.common.biz.user.UserContext;
+import com.kill4us.shortlink.admin.common.convention.exception.ClientException;
 import com.kill4us.shortlink.admin.common.convention.result.Result;
 import com.kill4us.shortlink.admin.dao.entity.GroupDO;
 import com.kill4us.shortlink.admin.dao.mapper.GroupMapper;
@@ -19,7 +21,12 @@ import com.kill4us.shortlink.admin.remote.ShortLinkRemoteService;
 import com.kill4us.shortlink.admin.remote.dto.resp.ShortLinkCountQueryRespDTO;
 import com.kill4us.shortlink.admin.service.GroupService;
 import com.kill4us.shortlink.admin.utils.RandomGenerator;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.redisson.api.annotation.REntity;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -29,18 +36,27 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 
+import static com.kill4us.shortlink.admin.common.constant.RedisCacheConstant.LOCK_GROUP_CREATE_KEY;
+
 /**
  * 短链接分组接口实现层
  */
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO> implements GroupService {
+
+    private final RedissonClient redissonClient;
+
+    @Value("${short-link.group.max-num}")
+    private Integer groupMaxNum;
 
     ShortLinkRemoteService shortLinkRemoteService = new ShortLinkRemoteService() {
     };
 
     /**
      * 新增短链接分组
+     *
      * @param groupName 分组名
      */
     @Override
@@ -50,24 +66,35 @@ public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO> implemen
 
     @Override
     public void saveGroup(String username, String groupName) {
-        String gid;
-        while (true) {
-            gid = RandomGenerator.generateRandom();
-            if (hasGid(username, gid)) {
-                break;
+        RLock lock = redissonClient.getLock(String.format(LOCK_GROUP_CREATE_KEY, username));
+        lock.lock();
+        try {
+            LambdaQueryWrapper<GroupDO> queryWrapper = Wrappers.lambdaQuery(GroupDO.class)
+                    .eq(GroupDO::getUsername, username)
+                    .eq(GroupDO::getDelFlag, 0);
+            List<GroupDO> groupDOList = baseMapper.selectList(queryWrapper);
+            if (CollUtil.isNotEmpty(groupDOList) && groupDOList.size() == groupMaxNum) {
+                throw new ClientException(String.format("已超出最大分组数：%d", groupMaxNum));
             }
+            String gid;
+            do {
+                gid = RandomGenerator.generateRandom();
+            } while (!hasGid(username, gid));
+            GroupDO groupDO = GroupDO.builder()
+                    .gid(gid)
+                    .sortOrder(0)
+                    .username(username)
+                    .name(groupName)
+                    .build();
+            baseMapper.insert(groupDO);
+        } finally {
+            lock.unlock();
         }
-        GroupDO groupDO = GroupDO.builder()
-                .name(groupName)
-                .username(username)
-                .sortOrder(0)
-                .gid(gid)
-                .build();
-        baseMapper.insert(groupDO);
     }
 
     /**
      * 检查gid是否可用
+     *
      * @param gid
      * @return 可用返回true
      */
@@ -81,6 +108,7 @@ public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO> implemen
 
     /**
      * 查询短链接分组
+     *
      * @return List集合
      */
     @Override
@@ -104,6 +132,7 @@ public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO> implemen
 
     /**
      * 修改短链接分组名
+     *
      * @param requestParam 分组名
      */
     @Override
@@ -119,6 +148,7 @@ public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO> implemen
 
     /**
      * 删除短链接分组
+     *
      * @param gid 分组ID
      */
     @Override
